42f0da534844d39ffce30deb30a5d8ee61c80a57
Ana Monsalve <91292255+amonsalv@users.noreply.github.com>
2023-09-07 13:17:27 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,6 @@
+function skillsMember() {
+    var member = document.getElementById("member");
+    var skillsMember = document.getElementById("skills-member");
+    member.style.display = "none";
+    skillsMember.style.display = "block";
+}
